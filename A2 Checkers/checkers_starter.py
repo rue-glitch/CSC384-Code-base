cf77138068311224dd6b2c42f5a7b320e2e67e17
Rue <rue.sriharsha@mail.utoronto.ca>
2023-02-21 17:14:53 -0500

    Added A2 starter files

---
+++ A2 Checkers/checkers_starter.py
@@ -0,0 +1,71 @@
+import argparse
+import copy
+import sys
+import time
+
+cache = {} # you can use this to implement state caching!
+
+class State:
+    # This class is used to represent a state.
+    # board : a list of lists that represents the 8*8 board
+    def __init__(self, board):
+
+        self.board = board
+
+        self.width = 8
+        self.height = 8
+
+    def display(self):
+        for i in self.board:
+            for j in i:
+                print(j, end="")
+            print("")
+        print("")
+
+def get_opp_char(player):
+    if player in ['b', 'B']:
+        return ['r', 'R']
+    else:
+        return ['b', 'B']
+
+def get_next_turn(curr_turn):
+    if curr_turn == 'r':
+        return 'b'
+    else:
+        return 'r'
+
+def read_from_file(filename):
+
+    f = open(filename)
+    lines = f.readlines()
+    board = [[str(x) for x in l.rstrip()] for l in lines]
+    f.close()
+
+    return board
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--inputfile",
+        type=str,
+        required=True,
+        help="The input file that contains the puzzles."
+    )
+    parser.add_argument(
+        "--outputfile",
+        type=str,
+        required=True,
+        help="The output file that contains the solution."
+    )
+    args = parser.parse_args()
+
+    initial_board = read_from_file(args.inputfile)
+    state = State(initial_board)
+    turn = 'r'
+    ctr = 0
+
+    sys.stdout = open(args.outputfile, 'w')
+
+    sys.stdout = sys.__stdout__
+
