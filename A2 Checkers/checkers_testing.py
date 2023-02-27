ff12c02d86a290f1d1ff901bf200cadbaa4b62cc
Rue <rue.sriharsha@mail.utoronto.ca>
2023-02-27 14:29:33 -0500

    Tested single and king r piece movements

--- A2 Checkers/checkers_testing.py
+++ A2 Checkers/checkers_testing.py
@@ -6,7 +6,7 @@ def test_read_file():
     filename = 'checkers2.txt'
     result = read_from_file(filename)
     actual = [['.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', 'b', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', 'R'], ['.', '.', 'b', '.', 'b', '.', '.', '.'], ['.', '.', '.', 'b', '.', '.', '.', 'r'], ['.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', 'r', '.', '.', '.', '.'], ['.', '.', '.', '.', 'B', '.', '.', '.']]
-    return actual == result
+    assert actual == result
 
 
 def test_grid_to_dict():
@@ -15,9 +15,8 @@ def test_grid_to_dict():
     board = Board(grid)
     pdict = board.grid_to_dict(grid)
     for key in pdict['b']:
-        if key in pdict['.']:
-            return False
-    return True
+        assert key not in pdict['.']
+
 
 
 def test_dict_to_grid():
@@ -26,5 +25,5 @@ def test_dict_to_grid():
     board = Board(grid)
     pdict = board.grid_to_dict(grid)
     generated_grid = board.dict_to_grid(pdict)
-    return grid == generated_grid
+    assert grid == generated_grid
 
